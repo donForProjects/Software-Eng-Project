7286027b0e0eab7438591f869829cf92fd82eb91
donForProjects <comiaforprojects@gmail.com>
2023-09-19 17:46:15 +0800

    50% code

--- window.py
+++ window.py
@@ -4,6 +4,8 @@ import cv2
 from PIL import Image, ImageTk
 import datetime
 from tkinter import ttk
+from datetime import datetime
+import pytz
 
 #VIDEO FRAME
 width, height = 600, 500
@@ -15,9 +17,21 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 root = tk.Tk()
 root.resizable(False, False)
 root.geometry("1417x720")
+IST = pytz.timezone('Asia/Manila')
 lmain = tk.Label(root)
-lmain.place(x=50, y=60)
+lmain.place(x=30, y=60)
 my_tree = ttk.Treeview(root)
+
+bg = PhotoImage(file="./banner.png")
+bg_label = Label(root, image=bg)
+bg_label.place(x=0, y=480, height=300)
+
+ico = Image.open('./logo.png')
+photo = ImageTk.PhotoImage(ico)
+root.wm_iconphoto(False, photo)
+
+root.title('Isabela State University Cauayan Campus')
+
 
 #TAKING PICS
 def snapshot():
@@ -36,18 +50,34 @@ def show_frame():
     lmain.configure(image=imgtk)
     lmain.after(5, show_frame)
 
+def update_clock():
+    raw_TS = datetime.now(IST)
+    date_now = raw_TS.strftime("%d %b %Y")
+    time_now = raw_TS.strftime("%I:%M:%S %p")
+    formatted_now = raw_TS.strftime("%d-%m-%Y")
+    label_date_now.config(text = date_now)
+    # label_date_now.after(500, update_clock)
+    label_time_now.config(text = time_now)
+    label_time_now.after(1000, update_clock)
+    return formatted_now
+
 #COLUMN TREE
 my_tree.tag_configure('orow', background="#EEEEEE", font=('Verdana', 10))
-my_tree.place(x=730, y=440, height=260)
+my_tree.place(x=730, y=60, height=260)
 
-label2 = tk.Label(root, text="ISU Student Registration", font=('Verdana', 20, 'bold'))
+label2 = tk.Label(root, text="ISU-CC INSTRUCTORS ATTENDANCE", font=('Verdana', 20, 'bold'))
 label2.place(x=500,y=0)
 
-Label(root, text="Instructors ID: ", font=('Verdana', 20, 'bold')).place(x=730, y=60)
-Label(root, text="Name: ", font=('Verdana', 20, 'bold')).place(x=730, y=200)
+Button(text="CHECK IN", command=snapshot, font=('Verdana', 15, 'bold'), width=15, height=1, bg='SpringGreen4', foreground='white').place(x=730, y=340)
+Button(text="CHECK OUT", font=('Verdana', 15, 'bold'), width=15, height=1, bg='SpringGreen4', foreground='white').place(x=1170, y=340)
 
-Button(text="Check In", command=snapshot, font=('Verdana', 15, 'bold'), width=15, height=1, bg='SpringGreen4').place(x=730, y=340)
-Button(text="Check Out", font=('Verdana', 15, 'bold'), width=15, height=1, bg='SpringGreen4').place(x=1150, y=340)
+label_time = tk.Label(root, text="Current Date and Time", font=('Verdana', 25, 'bold')).place(x=860, y=390)
+
+label_date_now = tk.Label(text="Current Date", font = 'Verdana 20 bold')
+label_date_now.place(x=730, y=450)
+
+label_time_now = tk.Label(text="Current Time", font = 'Verdana 20 bold')
+label_time_now.place(x=1150, y=450)
 
 style = ttk.Style()
 style.configure("Treeview.Heading", font=('Verdana', 13, 'bold'))
@@ -65,6 +95,6 @@ my_tree.heading("Name", text="Name", anchor=CENTER)
 my_tree.heading("Time In", text="Time In", anchor=CENTER)
 my_tree.heading("Time Out", text="Time Out", anchor=CENTER)
 
-
+update_clock()
 show_frame()
 root.mainloop()
